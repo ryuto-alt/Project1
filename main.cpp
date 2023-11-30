33532a7cae5165cba5341f6398d514584b73a469
uno_ryuto <k023g0078@g.neec.ac.jp>
2023-11-30 17:31:31 +0900

    ！

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ int main() {
 	std::cout << "Hello Github World\n";
 	std::cout << "PullRequest branch changed\n";
 	std::cout << "abc!\n";
-	std::cout << "PullRequest branch changed!!!!!!!\n";
+	std::cout << "PullRequest branch changed!!!!!\n";
 
 	return 0;
 }
