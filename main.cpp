bcb16c08c35f6ad662668a167825fe020db00a14
ryuto-alt <146914455+ryuto-alt@users.noreply.github.com>
2023-11-30 17:48:08 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,7 @@ int main() {
 	std::cout << "Hello Github World\n";
 	std::cout << "PullRequest branch changed\n";
 
-	std::cout << "abc!\n";
-	std::cout << "PullRequest branch changed!!!!\n";
-	std::cout << "PullRequest branch \n";
-
-	std::cout << "ab\n";
-	std::cout << "PullRequest branch changed\n";
-	std::cout << "PullRequest branch changed\n";
+	
 
 
 	return 0;
