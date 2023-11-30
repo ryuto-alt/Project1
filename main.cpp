6b10942c41b064f79ab93735a9cab0706a278257
uno_ryuto <k023g0078@g.neec.ac.jp>
2023-11-30 17:43:32 +0900

    aa

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include<stdio.h>
 #include<iostream>
 int main() {
+	std::cout << "Hello Github World\n";
+	std::cout << "PullRequest branch changed\n";
+
+	std::cout << "abc!\n";
+	std::cout << "PullRequest branch changed!!!!\n";
+	std::cout << "PullRequest branch \n";
 
 	std::cout << "ab\n";
+	std::cout << "PullRequest branch changed\n";
+	std::cout << "PullRequest branch changed\n";
+	std::cout << "PullRequest branch changed\n";
+	std::cout << "PullRequest branch changed\n";
+	std::cout << "PullRequest branch changed\n";
+	std::cout << "PullRequest branch changed\n";
+	std::cout << "PullRequest branch changed\n";
+	std::cout << "PullRequest branch changed\n";
+	std::cout << "PullRequest branch changed\n";
+	std::cout << "PullRequest branch changed\n";
+	std::cout << "PullRequest branch changed\n";
 
-  
+
 
 	return 0;
 }
